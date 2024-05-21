3ddbabd417b382311c2d9292d5065d8f9a1aacc1
Abhinav Varma <abhi.sjsu@gmail.com>
2024-05-21 21:23:38 +0530

    jenkins

--- check_tickets.py
+++ check_tickets.py
@@ -38,5 +38,29 @@ if response.status_code == 200:
             else:
                 print(f"Failed to close ticket {incident_number}: {close_response.status_code} - {close_response.text}")
             break
+
+        if ticket['active'] == 'true' and 'backup' in description:
+            inputs = '2\n6\n'
+            process = subprocess.Popen(['python3', 'main.py'], stdin=subprocess.PIPE, text=True)
+            process.communicate(inputs)
+            # After the process, close the ticket
+            incident_number = ticket['number']
+            close_url = f"https://dev263138.service-now.com/api/now/table/incident/{ticket['sys_id']}"
+            close_payload = {
+                "state": "7",
+                "active": "false",
+                "close_code": "Closed/Resolved by Caller",  # Adjust this field as necessary
+                "close_notes": "Automatically closed after processing"
+            }
+
+            close_response = requests.put(close_url, auth=(username, password), headers=headers,
+                                          data=json.dumps(close_payload))
+            if close_response.status_code == 200:
+                print(f"Ticket {incident_number} closed successfully.")
+            else:
+                print(
+                    f"Failed to close ticket {incident_number}: {close_response.status_code} - {close_response.text}")
+            break
+
 else:
     print("Failed to retrieve tickets:", response.status_code)
