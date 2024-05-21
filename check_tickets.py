12060c65d360d3afa65140e24ee5f8be88dde2c6
Abhinav Varma <abhi.sjsu@gmail.com>
2024-05-21 11:35:56 +0530

    jenkins

--- check_tickets.py
+++ check_tickets.py
@@ -4,8 +4,6 @@ password = 'A3%s!JbuhNO1'
 
 # ServiceNow API endpoint
 url = f"https://dev263138.service-now.com/api/now/table/incident"
-
-# Set up HTTP request headers
 headers = {
     "Content-Type": "application/json",
     "Accept": "application/json"
@@ -13,17 +11,11 @@ headers = {
 
 # Authenticate with ServiceNow
 response = requests.get(url, auth=(username, password), headers=headers)
-
+print(response)
 if response.status_code == 200:
     tickets = response.json().get("result", [])
     for ticket in tickets:
         description = ticket.get("short_description", "").lower()
-        if "upgrade vmanage" in description:
-            print("Upgrade vmanage script triggered for ticket:", ticket["number"])
-            # Write a flag to a file
-            with open('trigger_hello_world.txt', 'w') as f:
-                f.write('run')
-            break
+        print(description)
 else:
     print("Failed to retrieve tickets:", response.status_code)
-
