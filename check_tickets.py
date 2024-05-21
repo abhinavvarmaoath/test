144e34c2e35eb915baab202baf5699a4172fd78e
Abhinav Varma <abhi.sjsu@gmail.com>
2024-05-21 12:53:23 +0530

    jenkins

--- check_tickets.py
+++ check_tickets.py
@@ -13,14 +13,12 @@ headers = {
 
 # Authenticate with ServiceNow
 response = requests.get(url, auth=(username, password), headers=headers)
-print(response)
 if response.status_code == 200:
     tickets = response.json().get("result", [])
     for ticket in tickets:
         description = ticket.get("short_description", "").lower()
         if ticket['active'] == 'true' and 'changed state to down' in description:
             inputs = '5\n2\n3\n6\n'
-            # Execute the main.py script with the inputs
             process = subprocess.Popen(['python3', 'main.py'], stdin=subprocess.PIPE, text=True)
             process.communicate(inputs)
             break
